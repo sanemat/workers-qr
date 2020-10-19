cb78e033583bc6094b823d8b6a014e389f31b0b5
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-10-19 23:03:55 +0700

    chore(qrcode): use qrcode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import isRelativeUrl from "is-relative-url";
+import QRCode from "qrcode";
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
@@ -9,6 +10,14 @@ addEventListener("fetch", (event) => {
  */
 async function handleRequest(request: Request) {
   console.log(isRelativeUrl("foo/bar"));
+  // With promises
+  QRCode.toDataURL("I am a pony!")
+    .then((url) => {
+      console.log(url);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
   return new Response("Hello worker!", {
     headers: { "content-type": "text/plain" },
   });
