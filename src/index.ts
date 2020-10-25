89d0937c2f6f319fc136f5be1b2a73b9634ee978
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-10-25 10:18:52 +0700

    chore(deps): install qrnode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import isRelativeUrl from "is-relative-url";
+import qr from "qr-image";
+
+const generate = async (request: Request) => {
+  const { text } = await request.json();
+  const headers = { "Content-Type": "image/png" };
+  const qr_png = qr.imageSync(text || "https://workers.dev");
+  return new Response(qr_png, { headers });
+};
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
@@ -9,6 +17,7 @@ addEventListener("fetch", (event) => {
  */
 async function handleRequest(request: Request) {
   console.log(isRelativeUrl("foo/bar"));
+  await generate(request);
   return new Response("Hello worker!", {
     headers: { "content-type": "text/plain" },
   });
--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,12 @@ module.exports = {
     filename: "bundle.js",
   },
   target: ["web", "es2020"],
+  resolve: {
+    fallback: {
+      zlib: require.resolve("browserify-zlib"),
+      assert: require.resolve("assert/"),
+      buffer: require.resolve("buffer/"),
+      stream: require.resolve("stream-browserify"),
+    },
+  },
 };
