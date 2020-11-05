d40ad38c19039d9a7a358ab5c7939a8287468547
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-11-05 21:44:56 +0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,33 @@ import qr from "qr-image";
 import { PNG } from "pngjs/browser";
 
 const generate = async (request: Request) => {
+  let w = 32;
+  let h = 64;
+
+  /// RGBA input (color type 6)
+  let buffer = Buffer.alloc(2 * w * h * 4);
+  let bitmap = new Uint16Array(buffer.buffer);
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      bitmap[i * 4 * w + 4 * j] = (i * 65535) / h;
+      bitmap[i * 4 * w + 4 * j + 1] = (j * 65535) / w;
+      bitmap[i * 4 * w + 4 * j + 2] = ((h - i) * 65535) / h;
+      bitmap[i * 4 * w + 4 * j + 3] = 65535;
+    }
+  }
+
+  let png = new PNG({
+    width: w,
+    height: h,
+    bitDepth: 16,
+    colorType: 6,
+    inputColorType: 6,
+    inputHasAlpha: true,
+  });
+
   // const { text } = await request.json();
   const headers = { "Content-Type": "image/png" };
-  const qr_png = qr.imageSync("https://workers.dev");
+  const qr_png = PNG.sync.read(buffer);
   return new Response(qr_png, { headers });
 };
 
